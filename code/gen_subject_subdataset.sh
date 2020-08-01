e31050b353b22e04f16ad1c783466e4cbad1227f
Adam Richie-Halford <richiehalford@gmail.com>
2020-07-31 17:55:12 -0700

    Fix typo in gen_subject_subdataset.sh

--- code/gen_subject_subdataset.sh
+++ code/gen_subject_subdataset.sh
@@ -5,5 +5,5 @@ ALLSUBSCSV=$2
 OUTPUTDIR=$3
 
 for subid in $(cat $SUBLISTFILE); do
-    /usr/bin/grep "$subid" "$ALLSUBSCSV" > "$OUTPUTDIR/$subid.csv
+    /usr/bin/grep "$subid" "$ALLSUBSCSV" > "$OUTPUTDIR/$subid.csv"
 done
